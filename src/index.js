58c92ecdc70b0bd18fd16c37352d73de96177b4f
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2016-12-15 22:39:43 +0200

    Create the Header Component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {browserHistory} from 'react-router';
 import App from './components/Main';
 
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App history={browserHistory}/>, document.getElementById('app'));
